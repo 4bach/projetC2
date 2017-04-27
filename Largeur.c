cf52c6f7c5e643232d8b0605c39f02075c8ef752
jeandeb <johnydebod@gmail.com>
2017-04-27 15:17:39 +0200

    MAIN + LARGEUR

--- Largeur.c
+++ Largeur.c
@@ -8,6 +8,7 @@
 #include "Struct_File.h"
 #include "Largeur.h"
 
+// Calcul du nombre d'arc minimum
 int nbarcsmin(Graphe *G, int u, int v)
 { 
 
@@ -16,6 +17,7 @@ int nbarcsmin(Graphe *G, int u, int v)
   int tmp;
   int voisin;
 
+  // Tableau du nombre de tours effectués pour arriver à un point
   int* visit = ( int* )malloc( (G->nbsom + 1)*sizeof(int) ); 
   int i;
 
@@ -23,6 +25,7 @@ int nbarcsmin(Graphe *G, int u, int v)
 
   Init_file( &F );
 
+  // Initialisation du tableau visiter
   for ( i = 0; i < G->nbsom + 1; i++ ) 
     visit[i] = -1; 
 
@@ -30,18 +33,23 @@ int nbarcsmin(Graphe *G, int u, int v)
 
   enfile(&F, u);
 
+//Tant que la file n'est pas vide -> itérer
   while ( !(estFileVide(&F))) { 
 	tmp = defile( &F );
 	arr = G->T_som[tmp]->L_voisin;
+
+	//Dans chaque point du tableau, parcourir tous ses voisins
 	while ( arr != NULL ) { 
 		if( tmp == arr->a->v ) voisin = arr->a->u;
 		else voisin = arr->a->v;
 
+		//Si le point n'a pas été visité -> additioner 1 au nombre de visite de son père
 		if (visit[voisin] == -1 ) {
 			visit[voisin] = visit[tmp] + 1;
 			enfile(&F,voisin); 
 		}
 		
+		//Si ce point correponds à celui recherhé, renvoyer le nombre de "tours" effectués
 		if( voisin == v ) {
 			return visit[voisin];
 		}
@@ -57,13 +65,12 @@ ListeEntier cheminuv( Graphe *G, int u, int v )
 { 
 	
 	
-	//faire un tableau avec tous les elements, chaque case du tableau references la position de son pere
-	//si voisin == v , alors faire une boucle qui creer la liste des elements
-	// en allant progressivement vers son pere, 
 	Sommet *cour;
 	Cellule_arete* arr;
 	int tmp;
 	int voisin;
+
+	//Tableau ou chaque élément référence son père
 	int* chemin = ( int* )malloc( (G->nbsom + 1)*sizeof(int) ); 
 	int* visit = ( int* )malloc( (G->nbsom + 1)*sizeof(int) ); 
 	int i;
@@ -79,24 +86,27 @@ ListeEntier cheminuv( Graphe *G, int u, int v )
 
 	enfile(&F, u);
 
+	//Tant que la file n'est pas vide
 	while ( !(estFileVide(&F))) { 
+		
 		tmp = defile( &F );
-		//printf("1" );
 		arr = G->T_som[tmp]->L_voisin;
+
+		//On visite tous les voisins de chaque point tmp
 		while ( arr != NULL ) { 
-			//printf("2" );
+			
+			//Arrete visitée -> recherche du points correspondant
 			if( tmp == arr->a->v ) voisin = arr->a->u;
 			else voisin = arr->a->v;
 
+			//case du tableau chemin -> reference son père 
 			if (visit[voisin] == -1 ) {
-				//printf("voisin visité\n" );
 				visit[voisin] = visit[tmp] + 1;
 				chemin[voisin] = tmp;
 				enfile(&F,voisin); 
 			}
 			
 			if( voisin == v ) {
-				//printf("u == v \n" );
 				break;
 			}
 
@@ -105,12 +115,15 @@ ListeEntier cheminuv( Graphe *G, int u, int v )
 	
 
   }
+  	//Creation d'une liste des points du chemin
 	ListeEntier premier = malloc( sizeof(ListeEntier));
 	premier->u = v;
 	premier->suiv = NULL;
 	tmp = v;
 
 	while( tmp != u ){
+
+		//Dans la case tmp du tableau, il y a le numéro du point précedent
 		tmp = chemin[tmp];
 		ListeEntier deux = malloc( sizeof(ListeEntier));
 		deux->u = tmp;
@@ -120,6 +133,7 @@ ListeEntier cheminuv( Graphe *G, int u, int v )
 	return premier;
 }
 
+//Retourne une liste de tous les chemins correpondant aux commodités
 Liste_chemin* chemin_commodites( Graphe* G ) 
 {
 
@@ -129,10 +143,12 @@ Liste_chemin* chemin_commodites( Graphe* G )
 	int i;
 	Commod *liste_commodite = G->T_commod;
 
+
 	Liste_chemin* premier = malloc(sizeof( Liste_chemin ) );
 	premier->points = cheminuv( G, liste_commodite[i].e1, liste_commodite[i].e2 );
 	premier->suiv = NULL;
 	
+	//Calcul du chemin de u à v pour chaque commodité
 	for( i = 1; i<G->nbcommod; i++ ) {
 
 		Liste_chemin* deux = malloc(sizeof( Liste_chemin ) );
@@ -145,11 +161,18 @@ Liste_chemin* chemin_commodites( Graphe* G )
 	return premier;
 }
 
+//Ecritures de chaque chaine de points dans un fichier
 void ecrire_commodites( Graphe* G, char* nomfic ) 
-{
+{	
+	int cpt;
+  	cpt = nbarcsmin( G, 10, 15);
 	Liste_chemin* liste = chemin_commodites( G );
 	char str[10] = "";
 	FILE* f1;
+	if( ((f1=fopen(nomfic,"r"))!=NULL) ) {
+		remove(nomfic);
+		//return;
+	}
 	if(((f1=fopen(nomfic,"w"))==NULL)){
 		printf("Le fichier %s ne s'ouvre pas\n",nomfic);
 		return;
@@ -165,59 +188,20 @@ void ecrire_commodites( Graphe* G, char* nomfic )
 		fprintf(f1,"-1\n");
 		liste = liste->suiv;
 	}
+	fclose(f1);
 }
 
 
-/*int calcul_longueur( Graphe* G ) 
-{
-	int i;
-	int cpt = 0;
-	for( i = 1; i < G->nbsom; i++ ) {
-
-		Cellule_arete* tmp = G->T_som[i]->L_voisin;
-		while( tmp ) {
-			cpt+= tmp->a->longueur;
-			tmp = tmp->suiv;
-		}
-	}
-	return cpt/2;
-}*/
-
-
-
-/*int calcul_largeur( Graphe* G ) 
-{
-
-	int i;
-	int max = 0;
-
-	for( i = 1; i < G->nbsom; i++ ) {
-		Cellule_arete* tmp = G->T_som[i]->L_voisin;
-		while( tmp ) {
-			tmp->a->calc_gamma++;
-			tmp = tmp->suiv;
-		}
-	}
-
-	for( i = 1; i < G->nbsom; i++ ) {
-		Cellule_arete* tmp = G->T_som[i]->L_voisin;
-		while( tmp ) {
-			if( tmp->a->calc_gamma > max ) max = tmp->a->calc_gamma;
-			tmp = tmp->suiv;
-		}
-	}
-	return max;
-}*/
-
-//peut etre fait de maniere bcp + rapide -> on cherche un des points dans le tableau, puis on cherche l'autre dans ses voisins
+//Retourne l'arrête correpsondant à u et v dans le graphe
 Arete* recherche_arrete( Graphe* G, int u, int v ) {
 	int i;
 	Arete* a;
 	for( i = 1; i < G->nbsom+1; i++ ) {
-		//printf( "i = %d \n", i );
+
 		Cellule_arete* tmp = G->T_som[i]->L_voisin;
+
 		while( tmp ) {
-			//printf( "U = %d, V = %d, AU = %d, AV = %d\n", u, v, tmp->a->u, tmp->a->v );
+			//Si ce sont les mêmes numéros de points> return a
 			if( ( (tmp->a->u == u ) && ( tmp->a->v == v)) || ((tmp->a->v == u ) && (tmp->a->u == v) ) )
 				return tmp->a;
 			tmp = tmp->suiv;
@@ -226,6 +210,7 @@ Arete* recherche_arrete( Graphe* G, int u, int v ) {
 	return NULL;
 }
 
+//calcul de la largeur maximale 
 int calcul_largeur( char* nomfic, Graphe* G ) 
 {
 	int max = 0;
@@ -239,45 +224,52 @@ int calcul_largeur( char* nomfic, Graphe* G )
 		return 0;
 	}
 	
+	//On prend une première arrête (doute sur l'utilité)
 	sommet1 = GetEntier( f1 );
-	//printf( "sommet 1 = %d \n", sommet1 );
 	Skip( f1 );
 	sommet2 =  GetEntier( f1 );
-	//printf( "sommet 2 = %d \n", sommet2 );
 	a = recherche_arrete( G, sommet1, sommet2 );
 	a->calc_gamma++;
 
+	//Tant qu'on peut lire sommet 2 -> continue
 	while( sommet2 != 0 ) {
+
+		//On prend l'arrête suivante
 		sommet1 = sommet2;
 		Skip( f1 );
 		sommet2 = GetEntier( f1 );
-		//printf( "sommet 1 = %d \n", sommet1 );
-		//printf( "sommet 2 = %d \n", sommet2 );
-		//printf( "sommet1 = %d, sommet2 = %d\n", sommet1, sommet2 );
+
+		//Si sommet 2 correspond a la fin de la ligne -> on saute
 		if( sommet2 == -1 ) {
 			SkipLine( f1 );
-			//printf("\n");
 			continue;
+
+		//Ensuite sommet 1 =-1, on saute un tour
 		} else if( sommet1 == -1 ) continue;
-		//printf( "S1 = %d, S2 = %d ||", sommet1, sommet2 );
 		a = recherche_arrete( G, sommet1, sommet2 );
+
+		//A chaque fois que l'arrete est trouvée, on ajoute 1 à gamma
 		a->calc_gamma++;
+		if( a->calc_gamma > max ) max = a->calc_gamma;
 		
 	}
+
+	/*//On recherche le maximum
 	for( i = 1; i < G->nbsom; i++ ) {
-		//printf( "i = %d \n", i );
 		Cellule_arete* tmp = G->T_som[i]->L_voisin;
 		while( tmp ) {
 			if( tmp->a->calc_gamma > max ) max = tmp->a->calc_gamma;
 			tmp = tmp->suiv;
 		}
-	}
+	}*/
+	fclose(f1);
 	return max;
 }
 
-int calcul_longueur( char* nomfic, Graphe* G ) 
+//Même principe pour le calcul de longueur
+double calcul_longueur( char* nomfic, Graphe* G ) 
 {
-	int cpt = 0;
+	double cpt = 0;
 	int i;
 	FILE* f1;
 	int sommet1, sommet2;
@@ -304,10 +296,11 @@ int calcul_longueur( char* nomfic, Graphe* G )
 			continue;
 		} else if( sommet1 == -1 ) continue;
 
+		//Seul changement, on ajoute a chaque fois qu'on croise l'arrete, sa distance
 		a = recherche_arrete( G, sommet1, sommet2 );
 		cpt += a->longueur;
 	}
-	
+	fclose(f1);
 	return cpt;
 }
 
